e9759eb7ae51058c3ba682d4fe9e907254c16bce
Muhammad Hanzala <muhammadhanzala214@gmail.com>
2024-08-31 19:35:44 +0500

    Enemy Patrolling and Tower Shooting

---
+++ Assets/Scripts/TowerBullet.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class TowerBullet : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D rb;
+    private float timer;
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    
+        Vector3 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 1.5 ){
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.CompareTag("Player")){
+            Destroy(gameObject);
+            
+        }
+    }
+}
